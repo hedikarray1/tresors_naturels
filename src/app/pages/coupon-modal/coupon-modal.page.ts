504ece66b8ace2f59a8987d25629487cd10f6213
unknown <houssemeddine.gabsi@gmail.com>
2020-12-03 17:38:58 +0100

    update coupon modal

--- src/app/pages/coupon-modal/coupon-modal.page.ts
+++ src/app/pages/coupon-modal/coupon-modal.page.ts
@@ -18,22 +18,22 @@ export class CouponModalPage implements OnInit {
   allCoupons: any[] = [];
   MyCoupons: any[] = [];
 
+  loaded = false;
+
   constructor(private storage: Storage,
     private orderService: OrderService,
-   private clipboard: Clipboard,
+    private clipboard: Clipboard,
     public toastController: ToastController) { }
 
   ngOnInit() {
     this.getCoupons();
   }
-  ionViewDidEnter() {
-    //this.getUserData();
-    this.getCoupons();
-  }
+ 
 
   getCoupons1() {
     this.MyCoupons = [];
     this.allCoupons = [];
+    this.loaded = false;
     this.storage.get('auth-user').then((val) => {
       console.log('auth-user', val);
       this.orderService.getCountMyCoupons().then((data1: any[]) => {
@@ -46,55 +46,60 @@ export class CouponModalPage implements OnInit {
           this.allCoupons.forEach(element1 => {
             element1.meta_data.forEach(element => {
               if (element.key == "_acfw_loyalty_program_user" && element.value == val.id) {
-                element1.used=false;
-                element1.used_text="disponible";
+                element1.used = false;
+                element1.used_text = "disponible";
                 element1.used_by.forEach(element2 => {
-                  if(element2==val.id){
-                    element1.used=true;
-                    element1.used_text="utilisé";
+                  if (element2 == val.id) {
+                    element1.used = true;
+                    element1.used_text = "utilisé";
                   }
                 });
                 this.MyCoupons.push(element1);
+                this.loaded = true;
               }
             });
           });
-          console.log("mes coupons:",this.MyCoupons);
+          console.log("mes coupons:", this.MyCoupons);
         });
       });
 
     });
   }
 
-getCoupons(){
-  this.MyCoupons=[];
-  this.storage.get('auth-user').then((val) => {
-    console.log('auth-user', val);
-  this.orderService.getCountMyCouponsCustom(val.id).then((data:any[])=>{
-    this.MyCoupons=data;
-    this.MyCoupons.forEach((element)=>{
-    
-if(parseInt(element.usage_count)<parseInt(element.usage_limit))
-{
-  element.used=false;
-  element.used_text="disponible";
-}else{
-  element.used=true;
-  element.used_text="utilisé";
-}
+  getCoupons() {
+    this.MyCoupons = [];
+    this.loaded = false;
+    this.storage.get('auth-user').then((val) => {
+      console.log('auth-user', val);
+      this.orderService.getCountMyCouponsCustom(val.id).then((data: any[]) => {
+        this.MyCoupons = data;
+        this.MyCoupons.forEach((element) => {
+
+          if (parseInt(element.usage_count) < parseInt(element.usage_limit)) {
+            element.used = false;
+            element.used_text = "disponible";
+          } else {
+            element.used = true;
+            element.used_text = "utilisé";
+          }
+         
+         
+        });
+        this.loaded = true;
+        console.log("mes coupon",  this.MyCoupons);
+        console.log("loaded",  this.loaded);
+      });
     });
-
-  });
-  });
-}
+  }
 
 
- async copyToClipBoard(code){
+  async copyToClipBoard(code) {
     this.clipboard.copy(code);
-   const toast=await this.toastController.create({
-    message: 'Le code du coupon est copié avec succés',
-    duration: 2000
-  });
-  toast.present();
+    const toast = await this.toastController.create({
+      message: 'Le code du coupon est copié avec succés',
+      duration: 2000
+    });
+    toast.present();
 
   }
 
@@ -102,11 +107,10 @@ if(parseInt(element.usage_count)<parseInt(element.usage_limit))
   doRefresh(event) {
     console.log('Begin async operation');
     this.getCoupons();
-    setTimeout(() => {
 
-      console.log('Async operation has ended');
+   
       event.target.complete();
-    }, 2000);
+  
   }
 
 
